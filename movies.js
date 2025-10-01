96f1c313b5f36163cb47acf50195e439d2f0badd
chcharris763-tech <chcharris763@gmail.com>
2025-10-01 16:03:50 -0500

    errors

--- movies.js
+++ movies.js
@@ -1,25 +1,23 @@
-const moviesWrapper = document.querySelector('.movies')
+const moviesWrapper = document.querySelector('.movies');
 
 function searchChange(event) {
-    console.log(event.target.value)
+    renderMovies(event.target.value);
 }
 
 
-async function renderMovies() {
-    const response = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=fa23a09b&s=batman')
-    const data = await response.json()
-    const moviesArr = data.Search
-    console.log(moviesArr)
-    moviesWrapper.innerHTML = moviesArr.map((movie) => {
+async function renderMovies(searchTerm) {
+    const response = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=fa23a09b&s=${searchTerm}');
+    const data = await response.json();
+    const moviesArr = data.Search;
+    console.log(moviesArr);
+    moviesWrapper.innerHTML = moviesArr.slice(0, 6).map((movie) => {
         return `
         <div class="movie">
-            <img class="movie__img" src="">
+            <img class="movie__img" src="${movie.Poster}">
             <h3 class="movie__title">${movie.Title}</h3>
             <p class="movie__year">${movie.Year}</p>
             <button class="movie__button">Add to Watchlist</button>
         </div>
     `
-    })
+    }).join('');
 }
-
-renderMovies();
